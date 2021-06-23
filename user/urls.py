c81015a015437a2537607c9bc0909e0ee36ca33b
SanzharShadybekov <Shadubekovs777@gmail.com>
2021-06-23 21:32:41 +0600

    sanzhar add

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('list/', views.UserListView.as_view()),
+    path('detail/<int:pk>/', views.UserDetailView.as_view()),
+    path('update/<int:pk>/', views.UserUpdateView.as_view()),
+    path('delete/<int:pk>/', views.UserDestroyView.as_view()),
     path('register/', views.RegisterApiView.as_view()),
     path('login/', views.LoginApiView.as_view()),
     path('activate/<uuid:activation_code>', views.ActivationView.as_view(), name='activation_code')
